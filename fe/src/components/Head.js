5747fa28e40847417ff5583137523837f4e2da8f
Alari Verev <alariverev36@gmail.com>
2021-11-27 14:23:29 +0200

    added drawera and jsconfig for auto import

--- fe/src/components/Head.js
+++ fe/src/components/Head.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -6,23 +6,82 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import PersonIcon from '@mui/icons-material/Person';
+import SettingsIcon from '@mui/icons-material/Settings';
+import LeaderboardIcon from '@mui/icons-material/Leaderboard';
+import TocIcon from '@mui/icons-material/Toc';
+import HomeIcon from '@mui/icons-material/Home';
 import { Link } from "react-router-dom";
 import { Context } from "../store";
 import { loginUser } from "../store/actions";
 import { useNavigate } from "react-router-dom";
+import Drawer from '@mui/material/Drawer';
+import { Divider, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
 
 function Head() {
     const [state, dispatch] = useContext(Context);
     const navigate = useNavigate();
+    const [drawerOpen, setDrawerOpen] = useState(false);
 
     const logOut = () => {
         dispatch(loginUser({token:"",user : ""}));
         navigate('/login');
     }
 
+    const toggleDrawer = (open) => (event) => {
+      if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+        return;
+      }
+  
+      setDrawerOpen(open);
+    };
+
+    const iconsMapping = {
+      "Avaleht": <HomeIcon/>,
+      "Alagrupid": <TocIcon/>,
+      "Edetabel": <LeaderboardIcon/>,
+      "Minu andmed": <PersonIcon/>,
+      "Seaded": <SettingsIcon/>
+    }
+
     return (
         <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" color="primary" elevation="10">
+        <Drawer
+          anchor="left"
+          open={drawerOpen}
+          onClose={toggleDrawer(false)}
+          variant="temporary"
+        >
+          <Box
+            sx={{ width: 250 }}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+          >
+            <List>
+              {['Avaleht', 'Alagrupid', 'Edetabel'].map((text, index) => (
+                <ListItem button key={text}>
+                  <ListItemIcon>
+                    {iconsMapping[text]}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItem>
+              ))}
+            </List>
+            <Divider />
+            <List>
+              {['Minu andmed', 'Seaded'].map((text, index) => (
+                <ListItem button key={text}>
+                  <ListItemIcon>
+                    {iconsMapping[text]}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Drawer>
+        <AppBar position="static" color="primary" elevation={10}>
           <Toolbar>
             <IconButton
               size="large"
@@ -30,6 +89,7 @@ function Head() {
               color="inherit"
               aria-label="menu"
               sx={{ mr: 2 }}
+              onClick={toggleDrawer(true)}
             >
               <MenuIcon />
             </IconButton>
@@ -44,6 +104,20 @@ function Head() {
         </AppBar>
       </Box>
     )
+}
+
+const styles = {
+  root:{
+      padding: 50,
+      overFlow: "hidden"
+  },
+  drawer:{
+    width: "500px"
+  },
+  drawerItem: {
+    width: "500px",
+    margin: "10px 10px"
+  }
 }
 
 export default Head;
