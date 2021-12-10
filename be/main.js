30ee3bc73cb3082bbae27d69da9f490f6d975be6
Alari Verev <alari.verev@cooppank.ee>
2021-12-11 00:13:30 +0200

    getallteams, getuserpredictions, savepredictions

--- be/main.js
+++ be/main.js
@@ -219,6 +219,65 @@ app.put('/updatepoints/:id', async(req, res)=>{
 })
 
 
+// save PREDICTIONS
+app.post('/savepredictions/:userId', async(req, res) => {
+  try {
+
+    const { userId } = req.params;
+    // let {username="", pwhash=(new Date()).getTime().toString(15), firstname="", lastname="", email="", social_id="", social_platform="", profile_pic="",} = req.body;
+    console.log(req.body.scores)
+    console.log(`userdID: ${userId}`)
+
+    let updatedDataArr=[]
+    Object.keys(req.body.scores).map(async(matchId, row) => {
+      console.log("Matches row "+matchId)
+      console.log(req.body.scores[matchId])
+
+      const resp = await pool.query(
+        "SELECT * FROM predictions WHERE userid = $1 AND matchid = $2",
+        [userId, matchId]
+      );
+
+      if(resp.rows.length){
+        console.log("peab updatema")
+        console.log(`UPDATE predictions SET team1score = ${req.body.scores[matchId][1]}, team2score = ${req.body.scores[matchId][2]} WHERE userid = ${userId} AND matchid = ${matchId}`)
+        const updateData = await pool.query(
+          "UPDATE predictions SET team1score = $1, team2score = $2 WHERE userid = $3 AND matchid = $4",
+          [req.body.scores[matchId][1], req.body.scores[matchId][2], userId, matchId]
+        );
+        updatedDataArr.push("UPDATED MATCH "+matchId)
+      }else{
+
+        console.log("peab uue rea salvestama")
+        //kasutajat ei ole veel olemas
+        const newInsertion = await pool.query(
+          "INSERT INTO predictions (userid, matchid, team1score, team2score) VALUES($1,$2,$3,$4) RETURNING *",
+          [userId, matchId, req.body.scores[matchId][1], req.body.scores[matchId][2]]
+        );
+        updatedDataArr.push(newInsertion)
+      }
+    })
+    res.json({status: "OK", message:"Predictions updated", data: updatedDataArr});
+
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+
+// get user predicitons
+app.get('/getuserpredictions/:userId', async(req, res)=>{
+  try {
+    const { userId } = req.params;
+    const predictions = await pool.query("SELECT * FROM predictions WHERE userid = $1",[userId]);
+
+      res.json(predictions.rows);
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+
 // delete password
 app.delete('/deleteuser/:id', async(req, res)=>{
   try {
--- fe/src/views/Predict.js
+++ fe/src/views/Predict.js
@@ -1,14 +1,24 @@
 //import { Typography } from '@mui/material';
 import React, { useContext, useEffect, useState } from 'react';
-import { Box, Checkbox, FormControlLabel, Paper, Switch, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, TextField, Typography } from '@mui/material';
+import { Box, Button, Checkbox, FormControlLabel, Grid, Paper, Switch, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, TextField, Typography } from '@mui/material';
 import { Context } from '../store';
 import { blue } from '@mui/material/colors';
+import { updateTeams } from '../store/actions';
+import { ThemeContext } from '@mui/styled-engine';
 
 function Predict() {
     const [rows, setAllMatches] = useState([])
     const mappedTeams = {}
     const [allTeams, setAllTeams] = useState({})
+    const [loading, setLoading] = useState(false)
+    const [currentMatchID, setCurrentMatchID] = useState(null)
     const [state, dispatch] = useContext(Context);
+    const [scores, setScores] = useState({
+        1:{1:2, 2:4},
+        2:{1:4, 2:44},
+        3:{1:5, 2:2},
+        4:{1:0, 2:2}
+    })
 
     const columns = [
         { field: 'id', headerName: 'ID', width: 70 },
@@ -22,7 +32,30 @@ function Predict() {
         }
       ];
 
+
+
     useEffect(() => {
+
+        fetch('http://localhost:3001/getallteams', {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then(response => response.json())
+        .then(data => {
+            // console.log(JSON.stringify(data))
+            // console.log(data.length)
+            // setAllTeams(data)
+            dispatch(updateTeams(data))
+        }).then(() => {
+            makeTable()
+        })
+
+    },[])
+
+    const makeTable = () => {
+        console.log(state.teams.data)
+        setAllTeams(state.teams.data)
+
         const requestOptions = {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' }
@@ -30,31 +63,42 @@ function Predict() {
         fetch('http://localhost:3001/getallmatches', requestOptions)
         .then(response => response.json())
         .then(data => {
-            console.log(JSON.stringify(data))
-            console.log(data.length)
+            // console.log(JSON.stringify(data))
+            // console.log(data.length)
             setAllMatches(data);
+            teams()
+        }).then(()=>{
+            fetch(`http://localhost:3001/getuserpredictions/${state.auth.id}`, {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' }
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                // console.log(data.length)
+                // setAllTeams(data)
+                // dispatch(updateTeams(data))
 
-            
+                //TODO destruct data into ROWS??? allteams????
+            })
         })
-        setAllTeams(state.teams.data)
-
-        teams()
-    },[])
+    }
 
         function stableSort(array) {
             const stabilizedThis = array.map((el, index) => [el, index]);
-            console.log("sorting?")
-            console.log(stabilizedThis)
+            // console.log("sorting?")
+            // console.log(stabilizedThis)
             stabilizedThis.sort((a, b) => {
               return b[0].user_points - a[0].user_points;
             });
             return stabilizedThis.map((el) => el[0]);
         }
 
-        const teams = () => {
-            
+        const teams = () => { 
+            console.log("NYYD ALLES JOUDSIN SIIA.")
+            console.log(state.teams.data)
             Object.keys(allTeams).map((index)=>{
-                console.log(allTeams[index])
+                // console.log(allTeams[index])
                 mappedTeams[allTeams[index].id] = {team: allTeams[index].team, flag:allTeams[index].flag}
             })
             return mappedTeams
@@ -63,27 +107,53 @@ function Predict() {
             return no != '2' ? <div> <img src={url} width = "25" height="15" /> {name}</div> : <div>{name} <img src={url} width = "25" height="15" /> </div>
         }
 
+        const savePredictions = () => {
+            setLoading(true)
+            const data = JSON.stringify({ 
+                "scores": scores
+            })
+
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: data
+            };
+            fetch(`http://localhost:3001/savepredictions/${state.auth.id}`, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                // console.log(data)
+                
+                setLoading(false)
+                
+            })
+        }
+
         const matches = () => {
-            console.log("HERR")
+            // console.log("HERR")
    
-                 return   <TableContainer component={Paper} sx={styles.tableContainer}>
+                 return  <Grid item xs={12} sx={{textAlign: "start"}}>
+                  <TableContainer component={Paper} sx={styles.tableContainer}>
                                <Table sx={[styles.table, { minWidth: 650 }]} aria-label="simple table">
                                    <TableBody>
                                    {stableSort(rows).map((row) => {
+                                    //    console.log("SEE ON SEE")
+                                    //    console.log(scores)
                                        return <TableRow
                                                 className="matchRow"
                                                 key={row.id}
                                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                                 hover={true}
-                                                
                                                 >
                                                 <TableCell scope="row" size="small" sx={{}}>{allTeams && flagAndTeam((teams())[row.team1id].flag,(teams())[row.team1id].team)}</TableCell>
                                                 <TableCell align="right" size="small" sx={{width: "50px", padding:"5px", }}>
-                                                    <TextField id="outlined-basic" label="" variant="outlined" sx={{}}/>  
+                                                    <TextField id="outlined-basic" label="" variant="outlined" sx={{}} value={scores[row.id][1]} 
+                                                    onChange={(v) => {setScores({...scores,[row.id]:{...scores[row.id],1:v.target.value}})}}/>  
+                                                    
                                                 </TableCell>
                                                 <TableCell align="center" sx={{width:"10px", padding:"5px"}}>:</TableCell>
                                                 <TableCell align="left" size="small" sx={{width: "50px", padding:"5px"}}>
-                                                    <TextField id="outlined-basic" label="" variant="outlined" sx={{}}/>  
+                                                    <TextField id="outlined-basic" label="" variant="outlined" sx={{}} value={scores[row.id][2]}
+                                                    onChange={(v) => {setScores({...scores,[row.id]:{...scores[row.id],2:v.target.value}})}}/>  
                                                 </TableCell>
                                                 <TableCell align="right" size="small" sx={{}}>{allTeams && flagAndTeam((teams())[row.team2id].flag,(teams())[row.team2id].team,"2")}</TableCell>
                                                 </TableRow>
@@ -91,6 +161,8 @@ function Predict() {
                                    </TableBody>
                                </Table>
                            </TableContainer>
+                           <Button onClick={savePredictions} disabled={loading ? true : false} variant="contained" color="success" style={styles.btn}>Salvesta</Button>
+                        </Grid>
        } 
   
     return (
